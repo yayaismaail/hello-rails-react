3991754449610d7b6344c011e86125c24c02d3a1
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2024-02-08 08:18:58 +0100

    Update index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,25 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import { Controller } from "@hotwired/stimulus"
+import React from "react"
+import ReactDOM from 'react-dom'
+import { Provider } from "react-redux";
+
+import App from "../components/App"
+import store from "../store/store";
+
+// Connects to data-controller="react-router"
+export default class extends Controller {
+  connect() {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
+  }
+} 
